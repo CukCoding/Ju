b4ed556a3da48e7fa9d9fbff7aef50ed4b46ec8f
VictoryJu <kob0805@naver.com>
2021-02-18 23:26:57 +0900

    modify: 2999 최대 i 값 찾기

--- 7주차/2999.py
+++ 7주차/2999.py
@@ -2,26 +2,28 @@
 
 N = input()
 cnt = 0
-R=len(N)
-C=1
+tmp = 0
+R=1
+C=len(N)
 
 for i in range(1,len(N)): 
-    if len(N) <= 3: #4보다 작으면 2차원배열이 만들어지지 않음
-        R=1
-        C=len(N)
-    elif len(N) % i ==0: # R값이 가장큰 행렬을 구하기 위함
+    if len(N) % i ==0: # R값이 가장큰 행렬을 구하기 위함
         avg = len(N) / i # 1 * 16, 2 * 8 처럼 좌변값이 우변값을 나누는 형태임
-        if avg * i == len(N):
-            if i>=avg: #R값이 가장큰경우
-                R = i #열
-                C = int(avg) #행
+        if i >=avg: #R값이 가장큰경우
+            if i > avg: # R이 C보다 클경우 스왑
+                R = int(avg)
+                C = i
                 break
+            R = i #열
+            C = int(avg) #행
+            break
+    
 
 arr = [ [0] * C for _ in range(R)] # 2차원 배열 초기화
-print(arr)
+print(R,C,arr)
 
-for i in range(R): 
-    for j in range(C): # 10같은 경우에는 5*2배열이다. ㄴㄴ R<=C 위배되니 2*5가 맞음
+for i in range(C): 
+    for j in range(R): # 
         arr[j][i] = N[cnt] #세로로 값 입력하기 (암호화)
         cnt += 1 
 
