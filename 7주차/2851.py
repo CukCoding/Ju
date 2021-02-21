7f4b25e5587bccf079e98ba79d981e3f455b36d5
VictoryJu <kob0805@naver.com>
2021-02-21 22:18:53 +0900

    modify: 2851 sum[i] < 100 일떄 추가

--- 7주차/2851.py
+++ 7주차/2851.py
@@ -13,13 +13,17 @@ for i in range(10):
 for i in range(10):
     sum[i] = sum[i-1] + arr[i] #sum배열에 값을 누적시킨다.
     if sum[i] >= 100: #누적된값이 100보다 커질때
-        if sum[i] - 100 == 100 - sum[i-1]:
-            print(sum[i])
+        if sum[i] - 100 == 100 - sum[i-1]: #차이가 똑같을경우
+            print(sum[i]) #더 큰값을 출력
+            break
         elif sum[i] - 100 > 100 - sum[i-1]: # 현재값에서 100을 뺸 값과 100에서 과거값의 차이를 비교한다. 
             print(sum[i-1]) # 현재값에서 100을 뺸값이 더 크다는것은 과거값이 100에 더 근접하다는 의미이다.
+            break
         else:
             print(sum[i])
-        break
+            break
+    if i == 9 and sum[i] < 100:
+        print(sum[i])
 
 # for i in range(10):
 #     sum[i] = sum[i-1] + arr[i] #sum배열에 값을 누적시킨다.
