46fe737a83cb915cab63ce1dfd29095a16c2b2fa
VictoryJu <kob0805@naver.com>
2021-02-28 17:18:10 +0900

    solve: 2246

--- 8주차/2246.py
+++ 8주차/2246.py
@@ -1,12 +1,12 @@
 #x보다 가까우면 x보다 숙박비가 더 비싸다
 #x보다 숙박비가 싸면 x보다 멀다
-
+import sys
 N = int(input())
 
 arr =[[0] * 2 for _ in range(N) ]
 
 for i in range(N):
-    arr[i] = list(map(int,input().split()))
+    arr[i] = list(map(int,sys.stdin.readline().split())) #TimeOut으로 인해 sys모듈로 입력을 받는다.
 
 count = 0
 
