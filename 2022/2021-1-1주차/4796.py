6bd8764078398616d0e8d7cf00c1a734d7910475
win24-ju <win24@pgit.biz>
2022-01-06 19:30:21 +0900

    4796 풀이

--- 2022/2021-1-1주차/4796.py
+++ 2022/2021-1-1주차/4796.py
@@ -1,24 +1,17 @@
-L = int(input()) #L일동안만 사용가능
-P = int(input()) #P일
-V = int(input()) #휴가
+#L 일동안만 사용가능
+#캠핑일 P
+#휴가 일수 V
+# 20 / 8 = 2 * 5  10 + (20 % 8) = 14 
+#최소 일수(V/P)*L를 구하고 남은 일수를 더한다.
 
-sub = P-L
-vArr = []
-for i in range(0,V):
-    vArr.append(i+1)
-
-count = 0
-cnt = 0
-for i in range(0,V):
-    cnt +=1
+count = 1
+while True:
+    L, P, V = map(int, input().split())
+    if L == P == V == 0: 
+        break # 0 0 0입력시 종료
+ 
+    result = V//P * L 
+    result += min(V % P, L) # V%P 가 L보다 크면안됨 만약 크다면 V를 더해주어야함. 
+    print(f'Case {count}: {result}')
     count += 1
-    if count == L:
-        if cnt == V:
-            break
-        for i in range(0,sub):
-            vArr.pop()
-            print(vArr)
-        count = 0
-    
-print(vArr)
-        
+
