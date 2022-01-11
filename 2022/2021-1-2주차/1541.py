87366c4a45c5b0d3b917ebab178bda63edecc9b3
VictoryJu <kob0805@naver.com>
2022-01-11 22:39:53 +0900

    1541 풀이

--- 2022/2021-1-2주차/1541.py
+++ 2022/2021-1-2주차/1541.py
@@ -1,15 +1,12 @@
-strInput = input()
-isMinus = False
-num = 0
+strInput = input().split('-')
+
 sum = 0
-while(True):
-    i = 0
-    if strInput[i] != '-' and isMinus == False:
-        num += strInput[i]
-    if strInput[i] == '-' and isMinus == True:
-        isMinus = False
-        num-=sum
-        sum = 0
-    if strInput[i] == '-':
-        sum += strInput[i]
+for i in strInput[0].split('+'): # 첫번쨰- 가 나오기전까지의 수를 모두 더한것
+    sum += int(i)
+
+for i in strInput[1:]: #그 뒤로부터 다 빼주는데 +가 묶인애들도 있으므로 분리하기위해 +로 split 위에서 -기준으로 split했기에 +로묶였을거임
+    for j in i.split('+'):
+        sum -= int(j)
+
+print(sum)
 
